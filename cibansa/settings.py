79b9de0d451ce2bf86a867705f10868b4d2c3764
Ebuka Amaefula <amaefulacj@gmail.com>
2017-05-15 17:06:37 +0530

    Heroku push changes

--- cibansa/settings.py
+++ cibansa/settings.py
@@ -229,7 +229,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 #Local host setting
-# PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #
 # STATIC_ROOT = '/static/'
 # STATIC_URL = '/static/'
@@ -241,7 +241,7 @@ USE_TZ = True
 # )
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
