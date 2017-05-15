4585bdd926d01b5b9e9cf599e81e9f4b3d9f6727
Ebuka Amaefula <amaefulacj@gmail.com>
2017-05-15 18:32:57 +0530

    Heroku push changes

--- cibansa/settings.py
+++ cibansa/settings.py
@@ -239,8 +239,8 @@ USE_TZ = True
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_ROOT, 'static'),
 # )
-
-
+#
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
@@ -254,7 +254,7 @@ STATICFILES_DIRS = (
 MEDIA_ROOT =os.path.join(BASE_DIR,"media/")
 MEDIA_URL ='/media/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 
 
--- cibansa/wsgi.py
+++ cibansa/wsgi.py
@@ -13,6 +13,8 @@ from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cibansa.settings")
+
+# application = Cling(get_wsgi_application())
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
 
